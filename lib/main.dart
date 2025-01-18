03aadedfcee44e79f688da48d12a4c054b4991f9
raylyu <59774755+RayLyu-Mac@users.noreply.github.com>
2025-01-18 06:58:46 -0500

    update bundle name

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Color Memory Game',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
